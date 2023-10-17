28e80c0b7f719390244d399c72b1695dcad58d5d
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-16 22:57:53 -0400

    Added about me and fixed some routes

---
+++ Timothy's Portfolio/src/components/Data/ProjectData.js
@@ -0,0 +1,24 @@
+// Export and array of project objects
+
+
+export const projects = [
+
+  {
+    title: "Portfolio",
+    url: "link",
+    // image: imgPortfolio,
+    description:
+      "Challenge #2 for the University of Toronto Coding Bootcamp 2023. The purpose of this challenge is to build a portfolio.",
+    technologies: "HTML & CSS",
+    GitHubRepo: "LINK"
+  },
+  {
+    title: "NAME",
+    url: "URL",
+    image: "img",
+    description:
+      "DESCRIPTION ",
+    technologies: "HTML & CSS",
+    GitHubRepo: "LINK",
+  },
+];
---
+++ Timothy's Portfolio/src/components/Pages/About.js
@@ -0,0 +1,21 @@
+// Builds the component to show my headshot and a short bio.
+export default function Home() {
+    return (
+      <>
+        <div className="sections">
+          <section id="About">
+            <h2>About Me</h2>
+            <p>
+              Hello world! My name is Timothy, I am currently living in Toronto. 
+            </p>
+            <p>
+            As a recent graduate in the world of web development, I have immersed myself in the art of coding and found it to be not just a profession but a creative outlet. My journey has been filled with curiosity, countless lines of code, and a drive to craft solutions that make a real impact. From front-end design to back-end functionality, I have embraced the full spectrum of web development, and I am eager to showcase my projects that reflect my dedication and growth in this dynamic field. I am excited about what the future holds, and I am here to contribute my skills to exciting projects and collaborate with fellow enthusiasts. Welcome to my coding journey!
+            </p>
+            <p>
+              I recently completed the University of Toronto Coding Bootcamp in October 2023.
+            </p>
+          </section>
+        </div>
+      </>
+    );
+  }
