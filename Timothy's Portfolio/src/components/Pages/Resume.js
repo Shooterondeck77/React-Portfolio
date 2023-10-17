ca80915eaaae2a60ac171465145abebbe5383868
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-16 22:40:31 -0400

    added resume section

---
+++ Timothy's Portfolio/src/components/Pages/Resume.js
@@ -0,0 +1,29 @@
+
+export default function Resume() {
+  
+    return (
+      <div className="sections">
+        <section id="Contact">
+          <h2>Resume</h2>
+          <p>Download my <a href="/Resume/TTResume.pdf" alt="Resume" download>resume</a>.</p>
+            <h3>Front-End Proficiencies:</h3>
+            <ul>
+              <li>HTML, CSS, JavaScript</li>
+              <li>React</li>
+              <li>Leaflet, MapBox</li>
+              <li>Bootstrap, SASS</li>
+              <li>RESTful APIs</li>
+            </ul>
+            <h3>Back-End Proficiencies:</h3>
+            <ul>
+              <li>C#</li>
+              <li>ASP.NET</li>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>MS SQL, MySQL, MongoDB</li>
+              <li>Sequelize, Mongoose</li>
+            </ul>
+        </section>
+      </div>
+    );
+  }
